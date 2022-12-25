caa07c8bc4d2f9e6d30d6b10796c532a48d4ec74
4rmx <59803022+4rmx@users.noreply.github.com>
2022-12-25 21:34:09 +0700

    Create packer.lua

---
+++ nvim/lua/empty/packer.lua
@@ -0,0 +1,25 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+-- vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig' -- LSP config
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use 'navarasu/onedark.nvim'
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('mbbill/undotree')
+
+end)
+
